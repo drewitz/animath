972ca686abebd4213447a26600f9ad7c69c75a10
Simon Drewitz <simon.drewitz@gmail.com>
2022-02-02 22:28:13 +0000

    bugfix
    forgot the {} around the exponent

--- py/monome.py
+++ py/monome.py
@@ -12,7 +12,7 @@ class Potenz(Scene):
     def draw_logo(self):
         self.add(SVGMobject("stz-white.svg").scale_to_fit_width(0.3).to_corner(corner=RIGHT + DOWN))
     
-    n_max = 16
+    n_max = 13
 
     def add_timestamp(self):
         self.meta_data["timestamps"].append(self.renderer.time)
@@ -24,19 +24,19 @@ class Potenz(Scene):
         ax_labels = ax.get_axis_labels()
         self.add(ax, ax_labels)
         text = MathTex("f(x) = \ldots").shift(4*RIGHT+2*UP)
+        self.add(text)
 
         graph = None
         for n in range(self.n_max):
+            newtext = MathTex(f"f(x) = x^{{{n}}}").shift(text.get_center())
             if graph is not None:
                 new_graph = ax.plot(lambda x: x**n, x_range=[-2, 2], color="blue")
 
-                self.play(Transform(graph, new_graph))
+                self.play(Transform(graph, new_graph), Transform(text, newtext))
                 # graph = new_graph
             else:
                 graph = ax.plot(lambda x: x**n, color="blue")
-                self.play(FadeIn(graph))
-            newtext = MathTex(f"f(x) = x^{n}").shift(text.get_center())
-            self.play(Transform(text, newtext))
+                self.play(FadeIn(graph), Transform(text, newtext))
             self.add_timestamp()
 
         with open("data.json", "a") as f:
