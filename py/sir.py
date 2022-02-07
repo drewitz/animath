998971c0a79069b38d357eae4e68ec28295b6298
ST Drewitz <s.t.drewitz@dunelm.org.uk>
2022-02-07 16:57:49 +0100

    added meta_data to SIR

--- py/sir.py
+++ py/sir.py
@@ -1,6 +1,18 @@
 from modules.population import Population
+from modules.data_write import write_data
+
+tmax = 7
+
+meta_data = {
+    "title": "SIR Model",
+    "timestamps": [0, tmax],
+    "filename": "sir-model.mp4",
+    "path": "videos/"
+}
+
 
 pop = Population(3600, nrows=60)
-dest = pop.animate(tmax=6, dt=0.1)
+dest = pop.animate(tmax=tmax, dt=0.1)
 pop.show()
+write_data(meta_data)
 #dest.save("docs/videos/sir-model.mp4")
