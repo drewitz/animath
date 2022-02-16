158412b33d05c02c9ca7d75e1a9bf161e2303dc9
ST Drewitz <s.t.drewitz@dunelm.org.uk>
2022-02-16 15:24:52 +0100

    added automagic creation of small sir video

--- py/sir.py
+++ py/sir.py
@@ -10,9 +10,22 @@ meta_data = {
     "path": "videos/"
 }
 
+pop = Population(3600, nrows=60, bdies=[0, 25, 0, 25])
+dest = pop.animate(tmax=tmax, dt=0.1)
+#pop.show()
+write_data(meta_data)
+dest.save("docs/videos/sir-model.mp4")
+
+meta_data = {
+    "title": "SIR Model 100",
+    "timestamps": [0, tmax],
+    "filename": "sir-model-klein.mp4",
+    "path": "videos/"
+}
+
 
 pop = Population(100, nrows=10, bdies=[0, 7, 0, 7])
 dest = pop.animate(tmax=tmax, dt=0.1)
 #pop.show()
 write_data(meta_data)
-dest.save("docs/videos/sir-model.mp4")
+dest.save("docs/videos/sir-model-klein.mp4")
