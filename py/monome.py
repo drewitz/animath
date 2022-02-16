f28a8df9a1f30ca5dad39c74735d67ef0f358bf0
Simon Drewitz <s.t.drewitz@dunelm.org.uk>
2022-02-16 21:33:56 +0100

    added hyperbolas to potenzen

--- py/monome.py
+++ py/monome.py
@@ -13,7 +13,7 @@ class Potenz(Scene):
     def draw_logo(self):
         self.add(SVGMobject("stz-white.svg").scale_to_fit_width(0.3).to_corner(corner=RIGHT + DOWN))
     
-    n_max = 13
+    n_max = 9
 
     def add_timestamp(self):
         self.meta_data["timestamps"].append(self.renderer.time)
@@ -27,6 +27,7 @@ class Potenz(Scene):
         text = MathTex("f(x) = \ldots").shift(4*RIGHT+2*UP)
         self.add(text)
 
+
         graph = None
         for n in range(self.n_max):
             newtext = MathTex(f"f(x) = x^{{{n}}}").shift(text.get_center())
@@ -37,6 +38,24 @@ class Potenz(Scene):
                 # graph = new_graph
             else:
                 graph = ax.plot(lambda x: x**n, color="blue")
+                self.play(FadeIn(graph), Transform(text, newtext))
+            self.add_timestamp()
+
+        self.play(FadeOut(graph))
+        self.add_timestamp()
+
+        graph = None
+        for n in range(self.n_max):
+            newtext = MathTex(f"f(x) = x^{{{-n}}}").shift(text.get_center())
+            if graph is not None:
+                new_graph = VGroup(ax.plot(lambda x: x**(-n), x_range=[-6, -0.1**(1/n)], color="blue"),
+                                   ax.plot(lambda x: x**(-n), x_range=[0.1**(1/n), 6], color="blue"))
+
+                self.play(Transform(graph, new_graph), Transform(text, newtext))
+                # graph = new_graph
+            else:
+                graph = VGroup(ax.plot(lambda x: x**(-n), x_range=[-6, -0.01], color="blue"),
+                               ax.plot(lambda x: x**(-n), x_range=[0.01, 6], color="blue"))
                 self.play(FadeIn(graph), Transform(text, newtext))
             self.add_timestamp()
 
